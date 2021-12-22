48994a91a881073e07c06de0aab9deeff4476282
Nick Johnson <njohnson@ucsb.edu>
2021-12-22 12:10:03 -0800

    editing on the my first app

--- my_first_app2/pages/index.js
+++ my_first_app2/pages/index.js
@@ -1,9 +1,27 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Heading, Page, TextStyle, Layout, EmptyState } from "@shopify/polaris";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+class Index extends React.Component {
+  state = { open: false };
+  render() {
+    return (
+      <Page>
+        <Layout>
+          <EmptyState // Empty state component
+            heading="Discount your products temporarily"
+            action={{
+              content: 'Select products',
+              onAction: () => this.setState({ open: true }),
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>
+      </Page>
+    )
+  }
+};
 
 export default Index;
